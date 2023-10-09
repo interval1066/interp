3a369a98eede653228aa6ed5a4d40b6103b74d36
Tim ONeil <interval1066@gmail.com>
2023-10-08 20:14:18 -0700

    Got the one repor building on windows and linux

---
+++ windows/src/winfilesys.c
@@ -0,0 +1,41 @@
+#include "winfilesys.h"
+
+int 
+find_cfgfile(const char* file_name, char* path_found, bool bUseDot)
+{
+	return 0;
+}
+
+int
+create_cfgfile(const char* file_path)
+{
+	return 0;
+}
+
+void get_userdir(char* path)
+{
+}
+
+int write_motd(char* path, const char* motd)
+{
+	return 0;
+}
+
+int read_motd(const char* path)
+{
+	return 0;
+}
+const char* get_value(const char* key)
+{
+	return NULL;
+}
+
+int set_value(const char* key, char* value)
+{
+	return 0;
+}
+
+bool file_exists(char* path)
+{
+	return false;
+}
