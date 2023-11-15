065b8816969837c8ea394986e1705176530c7adb
Tim ONeil <interval1066@gmail.com>
2023-11-15 15:34:52 -0800

    Switching over to libconfig

--- windows/include/winfilesys.h
+++ windows/include/winfilesys.h
@@ -3,12 +3,21 @@
 #include <stdio.h>
 #include <stdbool.h>
 #include <windows.h>
+#include <locale.h>
 #include <Shlobj.h>
+#include <io.h>
 #include "support.h"
+#include "simple_strlib.h"
+#include "libconfig.h"
+
+#define F_OK 0
+#define access _access
 
 static LPCSTR sk = "SOFTWARE\\Spooge\\blah\\poop";
 PHKEY hk;
 char str_data[36];
+
+bool file_exists(const char*);
 
 int find_cfgfile(const char*, char*, bool);
 int create_cfgfile(const char*);
