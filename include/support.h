8704f04590e8f8b3670639c00be54911b41177f5
Tim ONeil <interval1066@gmail.com>
2023-11-15 15:34:16 -0800

    Switching over to libconfig

--- include/support.h
+++ include/support.h
@@ -24,19 +24,20 @@
 
 typedef enum
 {
+    CMD_MEMERR          = -11,
     CMD_NOOP            = -10,
-    CMD_INITNF          = -9,
-    CMD_HELPERR         = -8,
-    CMD_QUOTES          = -7,
-    CMD_IOERR           = -6,
-    CMD_BANNERPTH       = -5,
-    CMD_FILEEXST        = -4,
-    CMD_FILENF          = -3,
-    CMD_ARGS            = -2,
-    CMD_ERR             = -1,
-    CMD_OK              = 0,
-	CMD_QUIT			= 1
-} STATUS_CODE;
+    CMD_INITNF          = -9,   // initialzation file not found
+    CMD_HELPERR         = -8,   // problem with help subsystem
+    CMD_QUOTES          = -7,   // argument not enclosed in quotes
+    CMD_IOERR           = -6,   // io error
+    CMD_BANNERPTH       = -5,   //
+    CMD_FILEEXST        = -4,   // file exists
+    CMD_FILENF          = -3,   // file not found
+    CMD_ARGS            = -2,   // error with commnd arguments
+    CMD_ERR             = -1,   // non-specific error
+    CMD_OK              = 0,    // recognized command
+	CMD_QUIT			= 1     // exit application
+} RET_CODE;
 
 typedef int (*command_object)(char*);
 
