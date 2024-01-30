e50295fde7f0b00233ba327e0926098e577301c8
Timothy O'Neil <interval1066@gmail.com>
2024-01-29 21:40:07 -0800

    Error code returned when help file not found

--- src/intrinsic.c
+++ src/intrinsic.c
@@ -3,51 +3,54 @@
 int
 help(char* opts)
 {
-    int size;
-    char** splitresult = split(opts, ' ', &size);
+    int size, retcode;
+	char** splitresult = split(opts, ' ', &size);
+	
+	if(size == 1)
+        retcode = find_help_section(opts);
+	else
+        retcode = find_help_section(splitresult[1]);
 
-    if(size == 1)
-        find_help_section(opts);
-    else
-        find_help_section(splitresult[1]);
+	free(&splitresult[0]);
+    if(retcode != CMD_OK)
+        printf("Help file not found\n");
 
-    free(&splitresult[0]);
-
-    return CMD_OK;
+	return CMD_OK;
 }
 
 int
 find_help_section(char* section)
 {
-    bool found = false;
-    FILE* fp = NULL;
-    char cfg_path[MAXBUF], chunk[MAXBUF];
+	bool found = false;
+	FILE* fp = NULL;
+	char cfg_path[MAXBUF], chunk[MAXBUF];
 
-    memset(cfg_path, 0, sizeof(cfg_path));
+	memset(cfg_path, 0, sizeof(cfg_path));
 #ifndef _MSC_VER
-    get_userdir(cfg_path);
-    strcat(cfg_path, "/.interp.hlp");
+	get_userdir(cfg_path);
+	strcat(cfg_path, "/.interp.hlp");
 #else
-    get_userdir(cfg_path);
-    strcat(cfg_path, "\\.interp.hlp");
+	get_userdir(cfg_path);
+	strcat(cfg_path, "\\.interp.hlp");
 #endif
-    fp = fopen(cfg_path, "r");
-    if(!fp)
-        return CMD_FILEEXST;
+	fp = fopen(cfg_path, "r");
+    if(!fp) return CMD_FILEEXST;
 
-    while(fgets(chunk, sizeof(chunk), fp) != NULL) {
-        int n = strncmp(section, chunk, strlen(section));
+	while(fgets(chunk, sizeof(chunk), fp) != NULL) {
+		int n = strncmp(section, chunk, strlen(section));
 
-        if(n == 0) {
-            replace_char(chunk, '|', '\n');
-            fputs(chunk, stdout);
-            found = true;
-        }
-    }
-    if((!found) && strncmp(section, "?", (size_t)1) != 0)
-        printf("Keyword not found\n");
+		if(n == 0) {
+			replace_char(chunk, '|', '\n');
+			fputs(chunk, stdout);
+			found = true;
+		}
+	}
 
-    if(fp) fclose(fp);
+	if((!found) && strncmp(section, "?", (size_t)1) != 0)
+		printf("Keyword not found\n");
 
-    return CMD_OK;
+	if(fp) fclose(fp);
+	
+	return CMD_OK;
 }
+
