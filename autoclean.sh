8e5d5092e592d9c4977fd42a0b47df938aa4a42b
tim o'neil <interval1066@gmail.com>
2024-11-27 23:20:18 -0800

    added a long needed 'make clean' directive to the autoclean script

--- autoclean.sh
+++ autoclean.sh
@@ -2,6 +2,7 @@
 #
 # Clean autotools config, run in top directory
 #
+make clean
 rm -rf autom4te.cache
 rm aclocal.m4 compile config.h config.h.in config.log \
 	config.status configure depcomp install-sh Makefile.in \
